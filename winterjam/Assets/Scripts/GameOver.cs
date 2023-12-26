53a2cd90499df79923d25db4d2143204a41b0f44
Albert Zhan <albert.cfzhan@gmail.com>
2023-12-25 22:37:54 -0800

    time elapse

--- winterjam/Assets/Scripts/GameOver.cs
+++ winterjam/Assets/Scripts/GameOver.cs
@@ -1,31 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private GameObject text;
+    [SerializeField] private TextMeshProUGUI timeText;
+
+    private float _initialTime;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _initialTime = Time.time;
     }
 
     public void FinishGame()
     {
         gameObject.SetActive(true);
         text.SetActive(false);
+        double elapsedTime = Math.Round(Time.time - _initialTime, 2);
+        timeText.text = "Time elapsed: " + elapsedTime + " seconds";
+        GameObject.Find("Player").GetComponent<PlayerController>().enabled = false;
+        GameObject.Find("Score").GetComponent<Score>().enabled = false;
     }
     
     public void RestartButton()
     {
+        _initialTime = Time.time;
         SceneManager.LoadScene("Game");
     }
 
